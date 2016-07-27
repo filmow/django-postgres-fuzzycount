ca44d136edba9ab4ca3f3ce65740c00cbba45b73
Henrique Chehad <hchehad@gmail.com>
2016-07-27 17:41:30 -0300

    created cache feature

--- fuzzycount.py
+++ fuzzycount.py
@@ -3,15 +3,25 @@ from distutils.version import LooseVersion
 from django.conf import settings
 from django.db import connections
 from django.db.models import QuerySet, Manager
+
+from django.core.cache import cache
+
 import django
+import hashlib
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 DJANGO_VERSION_GTE_19 = LooseVersion(django.get_version()) \
                         >= LooseVersion('1.9')
 
+FUZZY_CACHE_ENABLED = getattr(settings, "FUZZY_CACHE_ENABLED", False)
+FUZZY_CACHE_TIME = getattr(settings, "FUZZYCOUNT_CACHE_TIME", 120)
+
 
 class FuzzyCountQuerySet(QuerySet):
-    def count(self):
+    def count(self, nocache=False):
         postgres_engines = ("postgis", "postgresql", "django_postgrespool")
         engine = settings.DATABASES[self.db]["ENGINE"].split(".")[-1]
         is_postgres = engine.startswith(postgres_engines)
@@ -24,8 +34,25 @@ class FuzzyCountQuerySet(QuerySet):
             is_filtered = self.query.where
         else:
             is_filtered = self.query.where or self.query.having
+
         if not is_postgres or is_filtered:
+            # Get the count or execute count() and save in cache
+            if FUZZY_CACHE_ENABLED and not nocache:
+                query_str = self.query.__str__()
+                qs_key = hashlib.md5(query_str).hexdigest()
+                count_cache = cache.get(qs_key)
+
+                if not count_cache:
+                    count = super(FuzzyCountQuerySet, self).count()
+                    count_cache = cache.set(qs_key, count, FUZZY_CACHE_TIME)
+                    if settings.DEBUG:
+                        logger.debug(
+                            "Creating cache for count: {}".format(query_str))
+                return count_cache
+
+            # Cache disabled
             return super(FuzzyCountQuerySet, self).count()
+
         cursor = connections[self.db].cursor()
         cursor.execute("SELECT reltuples FROM pg_class "
                        "WHERE relname = '%s';" % self.model._meta.db_table)
